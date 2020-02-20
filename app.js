9cdd5a889827dbbaeab4abe6e5308767ca8c3e7f
Wexira <svetlanawex@gmail.com>
2020-02-20 16:55:32 +0400

    add folder favicons and favicon.png

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ $(function () {
 	let intro = $("#intro");
 	let introH = intro.innerHeight();
 	let scrollPos = $(window).scrollTop();
-	// introH - это высота блока intro, 
-	// intro.height берет высоту блока без учета padding
-	// innerHeight - берет высоту блока с учетом padding.
+	let nav = $("#nav");
+	let navToggle = $("#navToggle");
+
+	/* introH - это высота блока intro, 
+	intro.height берет высоту блока без учета padding
+	innerHeight - берет высоту блока с учетом padding.*/
+
 
 	checkScroll(scrollPos, introH);
 
@@ -42,11 +46,40 @@ $(function () {
 		let elementOffset = $(elementId).offset().top;
 		/* $(elementId).offset(). - offset(отступ) данного элемента (elementId) от верха (top)  */
 
+		nav.removeClass("show");
+
 		console.log(elementOffset);
 
 		$("html, body").animate({
 			scrollTop: elementOffset - 70
 		}, 700);
 	});
+
+
+
+	/* Nav Toggle */
+
+
+	navToggle.on("click", function (event) {
+		event.preventDefault();
+
+		nav.toggleClass("show");
+	});
+
+
+	/* Reviews: https://kenwheeler.github.io/slick/ */
+
+	let slider = $("#reviewsSlider");
+
+	slider.slick({
+		infinite: true, /* прокрутка слайдеров, true - бесконечная прокрутка */
+		slidesToShow: 1, /* количество слайдеров которые будут показываться */
+		slidesToScroll: 1, /* количество слайдеров которые будут скролить */
+		fade: true, /* означает один исчезает, а другой появляется на его месте */
+		arrows: false, /* arrows: false (убирает стрелки), true показывает кнопки 
+		Previous/Next */
+		dots: true /* (показывает точки это количество слайдеров)*/
+	})
+
 
 });
